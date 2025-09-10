8930e486f2b4497868d37e3246bcfd1df028c5cf
mallinthbarakade18 <mbarakade2@gmail.com>
2025-09-10 16:19:59 +0530

    Explaing git & github

---
+++ JS/Codes/First/script.js
@@ -0,0 +1,15 @@
+/* this 
+    multi line comment */
+// this is single line comment
+
+
+// let a = +prompt("Enter a value");
+// let b = +prompt("Enter b value");
+// console.log("Sum is :"+(a+b));
+// console.log("Sub is :"+(a-b));
+// console.log("Prod is:"+(a*b));
+// console.log("Div is :"+(a/b));
+
+let l = +prompt("Enter l value");
+let b = +prompt("Enter b value");
+console.log("Area of Rectangle : "+(l*b));
