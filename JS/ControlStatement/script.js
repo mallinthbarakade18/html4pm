efd0ab41107c5a14911a42efba66f07a87d28d82
mallinthbarakade18 <mbarakade2@gmail.com>
2025-09-15 17:27:18 +0530

    Explaing git & github

---
+++ JS/ControlStatement/script.js
@@ -0,0 +1,62 @@
+// simple if
+// let x=+prompt("enter any value");
+// if(x>5){
+//     console.log(`${x} is grether then 5`);
+// }
+//if-else
+// let x=+prompt("enter any value");
+// if(x>=18){
+//     console.log(`eligible for voting`);
+// }else{
+//     console.log(`not eligible for voting`)
+// }
+// let age=+prompt("enter your age : ");
+// if(age>0 && age<=12){
+//     console.log(`Kid`);
+// }
+// else if(age>12 && age<=21){
+//     console.log(`teen ager`);
+// }
+// else if(age>21 && age<=30){
+//     console.log(`Young`);
+// }
+// else if(age>30 && age<=60){
+//     console.log(`mid age`);
+// }
+// else{
+//     console.log(`old`);
+// }
+
+//nested if:
+// let age=+prompt("enter your age : ");
+// let gender=+prompt("enter your gender : ");
+// if(age>20){
+//     if(gender==m){
+//         console.log(`male`);
+//     }
+// }
+
+
+// let n=+prompt("enter number : ");
+// let x=Math.ceil(Math.random()*14);
+// if(n==x){
+//     console.log(`you won`);
+// }
+// else if(n>x){
+//     console.log(`grether num`);
+// }
+// else{
+//     console.log(`less num`);
+// }
+
+let day=+prompt("enter day num : ");
+switch(day){
+    case 0: console.log(`Sunday`);break;
+    case 1: console.log(`Monday`);break;
+    case 2: console.log(`Tuesday`);break;
+    case 3: console.log(`Wednesday`);break;
+    case 4: console.log(`Thursday`);break;
+    case 5: console.log(`Friday`);break;
+    case 6: console.log(`Saturday`);break;
+    default: console.log(`invalid choice`);
+}
