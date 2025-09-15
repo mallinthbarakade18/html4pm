a808e9881b2b55b36609b88b253acb77afca2787
mallinthbarakade18 <mbarakade2@gmail.com>
2025-09-15 17:31:34 +0530

    Explaing git & github

--- JS/ControlStatement/script.js
+++ JS/ControlStatement/script.js
@@ -49,14 +49,15 @@
 //     console.log(`less num`);
 // }
 
-let day=+prompt("enter day num : ");
-switch(day){
-    case 0: console.log(`Sunday`);break;
-    case 1: console.log(`Monday`);break;
-    case 2: console.log(`Tuesday`);break;
-    case 3: console.log(`Wednesday`);break;
-    case 4: console.log(`Thursday`);break;
-    case 5: console.log(`Friday`);break;
-    case 6: console.log(`Saturday`);break;
-    default: console.log(`invalid choice`);
-}
+// let day=+prompt("enter day num : ");
+// switch(day){
+//     case 0: console.log(`Sunday`);break;
+//     case 1: console.log(`Monday`);break;
+//     case 2: console.log(`Tuesday`);break;
+//     case 3: console.log(`Wednesday`);break;
+//     case 4: console.log(`Thursday`);break;
+//     case 5: console.log(`Friday`);break;
+//     case 6: console.log(`Saturday`);break;
+//     default: console.log(`invalid choice`);
+// }
+
