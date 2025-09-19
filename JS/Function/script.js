c69fe649d0fd9f2057edffd8f699feba510e9347
mallinthbarakade18 <mbarakade2@gmail.com>
2025-09-19 17:40:58 +0530

    Explaing git & github

--- JS/Function/script.js
+++ JS/Function/script.js
@@ -1,11 +1,3 @@
-
-                    function outer(){
-                        let a=5;
-                        function inner(){
-                            console.log(a);
-                        }
-                        return inner;
-                    }
-                    console.log(outer());
-                    
-                    
+(function(){
+                console.log(`hi`);
+            })();
